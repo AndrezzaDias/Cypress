352beb879c069dcc53b8429d123aa34503ff871a
Andreza Dias <61808213+AndrezzaDias@users.noreply.github.com>
2022-07-27 22:07:16 -0300

    Cypress

---
+++ alura-fotos/cypress/e2e/alura-fotos.cy.js
@@ -0,0 +1,11 @@
+describe('Login e registro do alura pic', () => {
+
+    beforeEach(() => {
+      cy.visit('https://alura-fotos.herokuapp.com/#/home')
+
+    })
+
+     it('Verifica mensagem validacao', () => {
+    
+    })
+})
